ff232de37ced9a2cf43cfa3da24a2ecdf242f503
Elmira Ilyasova <capricorn_26@mail.ru>
2022-08-04 21:22:24 +0700

    Add Search component

---
+++ src/components/posts/search/search.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+const Search = (): JSX.Element => {
+  return (
+    <div className="search">
+      <input className="search__input" type="text" placeholder="Поиск"/>
+    </div>
+  )
+};
+
+export default Search;
